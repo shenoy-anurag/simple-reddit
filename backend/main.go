12a9e2a6a61763250e6414c9c7c6fcce834695cd
Ganesan Santhanam <gsanthanam@ufl.edu>
2022-01-13 19:06:56 -0500

    changes made to url

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/simplereddit", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
