c7fc61a58f0995d1f32207628378c4427bf15c72
John <johnng@ufl.edu>
2022-01-17 21:39:55 -0500

    fix: changed home page to include updating posts

--- frontend/forum-app/src/app/posts/posts.component.ts
+++ frontend/forum-app/src/app/posts/posts.component.ts
@@ -3,14 +3,6 @@ import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
-  template: `
-        <h2>{{title}}</h2>
-        <ul>
-             <li *ngFor="let post of posts">
-                 {{ post }}
-             </li>
-        </ul>
-    `,
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
