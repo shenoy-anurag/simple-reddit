94c0071c72d4b8da37931f03eb8d8b60e98f26bd
Ganesan Santhanam <ganesan.santhanam96@gmail.com>
2022-02-27 20:17:14 -0500

    feat:returning user details in the login response

--- backend/users/models.go
+++ backend/users/models.go
@@ -4,28 +4,31 @@ import (
 	"time"
 )
 
-// Multiple models in order to reduce coupling, implement single responsibility, 
+// Multiple models in order to reduce coupling, implement single responsibility,
 // and to emulate private class variables in Go.
 type CreateUserRequest struct {
-	Name     string `json:"name,omitempty" validate:"required"`
-	Email    string `json:"email" validate:"required"`
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	FirstName string `json:"firstname,omitempty"`
+	LastName  string `json:"lastname,omitempty"`
+	Email     string `json:"email" validate:"required"`
+	Username  string `json:"username" validate:"required"`
+	Password  string `json:"password" validate:"required"`
 }
 
 type UserResponse struct {
-	Name     string    `json:"name,omitempty"`
-	Email    string    `json:"email"`
-	Username string    `json:"username"`
-	Joined   time.Time `bson:"joined"`
+	FirstName string    `json:"firstname,omitempty"`
+	LastName  string    `json:"lastname,omitempty"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Joined    time.Time `bson:"joined"`
 }
 
 type UserDBModel struct {
-	Name     string    `json:"name,omitempty"`
-	Email    string    `json:"email"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
-	Joined   time.Time `bson:"joined"`
+	FirstName string    `json:"firstname,omitempty"`
+	LastName  string    `json:"lastname,omitempty"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Joined    time.Time `bson:"joined"`
 }
 
 type LoginUserRequest struct {
@@ -37,13 +40,27 @@ type CheckUsernameRequest struct {
 	Username string `json:"username" validate:"required"`
 }
 
+// type ProfileDBModel struct {
+// }
+
 // Convertion functions to convert between different models.
 func ConvertUserRequestToUserDBModel(usrReq CreateUserRequest) UserDBModel {
 	return UserDBModel{
-		Name:     usrReq.Name,
-		Email:    usrReq.Email,
-		Username: usrReq.Username,
-		Password: usrReq.Password,
-		Joined:   time.Now().UTC(),
+		FirstName: usrReq.FirstName,
+		LastName:  usrReq.LastName,
+		Email:     usrReq.Email,
+		Username:  usrReq.Username,
+		Password:  usrReq.Password,
+		Joined:    time.Now().UTC(),
+	}
+}
+
+func ConvertUserDBModelToUserResponse(userDB UserDBModel) UserResponse {
+	return UserResponse{
+		FirstName: userDB.FirstName,
+		LastName:  userDB.LastName,
+		Email:     userDB.Email,
+		Username:  userDB.Username,
+		Joined:    userDB.Joined,
 	}
 }
