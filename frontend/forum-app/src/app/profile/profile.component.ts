131162e8a3cfea587c7ee4264e3a54a5f49c4853
NgJohn15 <ngjohn15@gmail.com>
2022-03-01 16:10:34 -0500

    feat(profile): added basic JohnDoe example profile

--- frontend/forum-app/src/app/profile/profile.component.ts
+++ frontend/forum-app/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  profile;
+  constructor(service: ProfileService) {
+    this.profile = service.getProfile();
+   }
 
   ngOnInit(): void {
   }
