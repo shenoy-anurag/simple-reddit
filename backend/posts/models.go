f3774edcf7a43d75dbdfb40366e2f4f6c5ee350f
Ganesan Santhanam <ganesan.santhanam96@gmail.com>
2022-03-02 11:13:05 -0500

    feat: GetAllPosts API to fetch all posts for specific user in a community

--- backend/posts/models.go
+++ backend/posts/models.go
@@ -1,7 +1,6 @@
 package posts
 
 import (
-	"simple-reddit/users"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -10,8 +9,9 @@ import (
 // Multiple models in order to reduce coupling, implement single responsibility,
 // and to emulate private class variables in Go.
 type PostDBModel struct {
+	ID          primitive.ObjectID `bson:"_id"`
 	CommunityID primitive.ObjectID `bson:"community_id"`
-	UserID      primitive.ObjectID `bson:"user_id"`
+	UserName    string             `bson:"username"`
 	Title       string             `bson:"title"`
 	Body        string             `json:"body"`
 	Upvotes     int                `bson:"upvotes"`
@@ -26,16 +26,42 @@ type CreatePostRequest struct {
 	Body        string             `json:"body"`
 }
 
+type GetPostRequest struct {
+	UserName    string             `json:"username,required"`
+	CommunityID primitive.ObjectID `json:"community_id,required"`
+}
+
+type PostResponse struct {
+	ID        primitive.ObjectID `json:"_id"`
+	Title     string             `json:"title"`
+	Body      string             `json:"body"`
+	Upvotes   int                `json:"upvotes"`
+	Downvotes int                `json:"downvotes"`
+	CreatedAt time.Time          `json:"created_at"`
+}
+
 // Convertion functions to convert between different models.
-func ConvertPostRequestToPostDBModel(postReq CreatePostRequest) (PostDBModel, error) {
-	userDB, err := users.GetUserDetails(postReq.UserName)
+func ConvertPostRequestToPostDBModel(postReq CreatePostRequest) PostDBModel {
 	return PostDBModel{
+		ID:          primitive.NewObjectID(),
 		CommunityID: postReq.CommunityID,
-		UserID:      userDB.ID,
+		UserName:    postReq.UserName,
 		Title:       postReq.Title,
 		Body:        postReq.Body,
-		Upvotes:     0,
+		Upvotes:     1,
 		Downvotes:   0,
 		CreatedAt:   time.Now().UTC(),
+	}
+}
+
+func ConvertPostDBModelToPostResponse(postDB PostDBModel) (PostResponse, error) {
+	var err error
+	return PostResponse{
+		ID:        postDB.ID,
+		Title:     postDB.Title,
+		Body:      postDB.Body,
+		Upvotes:   postDB.Upvotes,
+		Downvotes: postDB.Downvotes,
+		CreatedAt: postDB.CreatedAt,
 	}, err
 }
--- backend/posts/service.go
+++ backend/posts/service.go
@@ -2,16 +2,19 @@ package posts
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"simple-reddit/configs"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-const POST_ROUTE_PREFIX = "/posts"
+const POST_ROUTE_PREFIX = "/post"
 
 const PostsCollectionName string = "posts"
 
@@ -64,10 +67,73 @@ func CreatePost() gin.HandlerFunc {
 	}
 }
 
+func GetPosts() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var postReq GetPostRequest
+
+		// validate the request body
+		if err := c.BindJSON(&postReq); err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				configs.APIResponse{
+					Status:  http.StatusBadRequest,
+					Message: configs.API_FAILURE,
+					Data:    map[string]interface{}{"data": err.Error()}},
+			)
+			return
+		}
+		fmt.Println("bind passed")
+		// use the validator library to validate required fields
+		if validationErr := validate.Struct(&postReq); validationErr != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				configs.APIResponse{
+					Status:  http.StatusBadRequest,
+					Message: configs.API_FAILURE,
+					Data:    map[string]interface{}{"data": validationErr.Error()}},
+			)
+			return
+		}
+		fmt.Println("validation done")
+		postDetails, err := retrievePostDetails(postReq)
+		fmt.Println(postDetails)
+		if err != nil {
+			c.JSON(
+				http.StatusInternalServerError,
+				configs.APIResponse{
+					Status:  http.StatusInternalServerError,
+					Message: configs.API_ERROR,
+					Data:    map[string]interface{}{"data": err.Error()}},
+			)
+			return
+		}
+
+		if len(postDetails) > 0 {
+			c.JSON(
+				http.StatusOK,
+				configs.APIResponse{
+					Status:  http.StatusOK,
+					Message: configs.API_SUCCESS,
+					Data:    map[string]interface{}{"posts": postDetails}},
+			)
+			return
+		} else {
+			c.JSON(
+				http.StatusOK,
+				configs.APIResponse{
+					Status:  http.StatusNotFound,
+					Message: configs.API_SUCCESS,
+					Data:    map[string]interface{}{"posts": postDetails}},
+			)
+			return
+		}
+	}
+}
+
 func createPostInDB(post CreatePostRequest) (result *mongo.InsertOneResult, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	newPost, err := ConvertPostRequestToPostDBModel(post)
+	newPost := ConvertPostRequestToPostDBModel(post)
 	if err != nil {
 		return result, err
 	}
@@ -75,6 +141,32 @@ func createPostInDB(post CreatePostRequest) (result *mongo.InsertOneResult, err 
 	return result, err
 }
 
+func retrievePostDetails(postReq GetPostRequest) ([]PostResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var posts []PostDBModel
+	var postResp []PostResponse
+	filter := bson.D{primitive.E{Key: "community_id", Value: postReq.CommunityID}, primitive.E{Key: "username", Value: postReq.UserName}}
+	cursor, err := postCollection.Find(ctx, filter)
+	if err = cursor.All(ctx, &posts); err != nil {
+		return postResp, err
+	}
+	fmt.Println("posts")
+	fmt.Println(posts)
+	for _, post := range posts {
+		item, err := ConvertPostDBModelToPostResponse(post)
+		if err != nil {
+			return postResp, err
+		}
+		fmt.Print(item)
+		postResp = append(postResp, item)
+
+	}
+	return postResp, err
+}
+
 func Routes(router *gin.Engine) {
 	router.POST(POST_ROUTE_PREFIX+"/create", CreatePost())
+	router.GET(POST_ROUTE_PREFIX+"/get", GetPosts())
+
 }
