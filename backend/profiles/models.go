591ae91cd41e096a777adb5f6e888bcb9bcf0ab0
Ganesan Santhanam <ganesan.santhanam96@gmail.com>
2022-03-04 13:44:02 -0500

    feat: profile details for a user can be fetched using GetProfile API

--- backend/profiles/models.go
+++ backend/profiles/models.go
@@ -10,7 +10,29 @@ type ProfileDBModel struct {
 	FirstName string             `json:"firstname,omitempty"`
 	LastName  string             `json:"lastname,omitempty"`
 	Email     string             `json:"email"`
-	Username  string             `json:"username"`
+	UserName  string             `json:"username"`
 	Karma     int                `json:"karma"`
 	Birthday  time.Time          `bson:"birthday"`
+}
+
+type GetProfileRequest struct {
+	UserName    string             `json:"username" validate:"required"`
+}
+
+type ProfileResponse struct {
+	FirstName string    `json:"firstname,omitempty"`
+	LastName  string    `json:"lastname,omitempty"`
+	Email     string    `json:"email"`
+	UserName  string    `json:"username"`
+	Birthday    time.Time `json:"joined"`
+}
+
+func ConvertProfileDBModelToProfileResponse(profileDB  ProfileDBModel) ProfileResponse {
+	return ProfileResponse{
+		FirstName: profileDB.FirstName,
+		LastName:  profileDB.LastName,
+		Email:     profileDB.Email,
+		UserName:  profileDB.UserName,
+		Birthday: profileDB.Birthday,
+	}
 }
