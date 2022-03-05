423e7cad329ecd643dcf003a576982015d8e6d72
NgJohn15 <ngjohn15@gmail.com>
2022-03-04 23:43:12 -0500

    fix: cleaned up spec for newsubbredditforms

--- frontend/forum-app/src/app/newsubredditsform/newsubredditsform.component.spec.ts
+++ frontend/forum-app/src/app/newsubredditsform/newsubredditsform.component.spec.ts
@@ -18,8 +18,4 @@ describe('NewsubredditsformComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
