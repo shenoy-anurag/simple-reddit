94d535c974699b66781516498b8998da12479a43
Manoj_Jampana <50446748+manojjampana@users.noreply.github.com>
2022-04-14 20:25:04 -0400

    feat: Added contact info

--- frontend/forum-app/src/app/termsandconditions/termsandconditions.component.ts
+++ frontend/forum-app/src/app/termsandconditions/termsandconditions.component.ts
@@ -37,6 +37,27 @@ scrollToTop() {
   })();
 }
 
+copyMessage(val: string){
+  const selBox = document.createElement('textarea');
+  selBox.style.position = 'fixed';
+  selBox.style.left = '0';
+  selBox.style.top = '0';
+  selBox.style.opacity = '0';
+  selBox.value = val;
+  document.body.appendChild(selBox);
+  selBox.focus();
+  selBox.select();
+  document.execCommand('copy');
+  document.body.removeChild(selBox);
+}
+
+public showMyMessage = false
+
+showMessageSoon() {
+  setTimeout(() => {
+    this.showMyMessage = true
+  }, 1000)
+}
 
   ngOnInit(): void {
   }
