3532b54f73c2d2bb9cac05b41b3d7db420aa5772
Manoj_Jampana <50446748+manojjampana@users.noreply.github.com>
2022-04-17 01:11:24 -0400

    feat:added back to top option for privacy policy page

--- frontend/forum-app/src/app/privacypolicy/privacypolicy.component.ts
+++ frontend/forum-app/src/app/privacypolicy/privacypolicy.component.ts
@@ -9,7 +9,32 @@ import { DOCUMENT } from '@angular/common';
 })
 export class PrivacypolicyComponent implements OnInit {
 
-  constructor() { }
+  windowScrolled!: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener("window:scroll", [])
+
+onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+        this.windowScrolled = true;
+    } 
+   else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+        this.windowScrolled = false;
+    }
+}
+
+
+scrollToTop() {
+  (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+          window.requestAnimationFrame(smoothscroll);
+          window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
+  })();
+}
+
 
   copyMessage(val: string){
     const selBox = document.createElement('textarea');
