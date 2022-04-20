5c305d29a6ddf5533dc8e066d40581a57c0fa40a
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-04-20 01:49:08 -0400

    test: added profile CRUD tests

--- backend/tests/profile_test.go
+++ backend/tests/profile_test.go
@@ -14,14 +14,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSignup(t *testing.T) {
+func TestSignupForProfile(t *testing.T) {
 	var resp = common.APIResponse{}
 	var body = users.CreateUserRequest{
-		FirstName: "Mary",
-		LastName:  "Jane",
-		Email:     "mary.jane@ufl.edu",
-		Username:  "mary_jane",
-		Password:  "password123",
+		FirstName: "Albert",
+		LastName:  "Einstein",
+		Email:     "einsteinalbert@ufl.edu",
+		Username:  "albert",
+		Password:  "special-relativity",
 	}
 	var expected = common.APIResponse{
 		Status:  http.StatusCreated,
@@ -55,33 +55,58 @@ func TestSignup(t *testing.T) {
 	assert.Contains(t, resp.Data, "data")
 }
 
-func TestLogin(t *testing.T) {
+func TestGetProfile(t *testing.T) {
 	var resp = common.APIResponse{}
-	var body = users.LoginUserRequest{
-		Username: "mary_jane",
-		Password: "password123",
+	var body = profiles.GetProfileRequest{
+		UserName: "albert",
 	}
-	userDB, _ := users.GetUserDetails(body.Username)
-	userResp := users.ConvertUserDBModelToUserResponse(userDB)
 	var expected = common.APIResponse{
 		Status:  http.StatusOK,
 		Message: common.API_SUCCESS,
-		Data:    map[string]interface{}{"accessToken": "", "user": userResp},
+		Data:    map[string]interface{}{},
 	}
 
-	// // isoDateTime, err := time.Parse("YYYY-mm-ddTHH:MM:ssZ", "2022-01-21T04:41:56.616Z")
-	// var user = users.CreateUserRequest{
-	// 	FirstName: "Albert",
-	// 	LastName:  "Einstein",
-	// 	Email:     "einsteinalbert@ufl.edu",
-	// 	Username:  "albert",
-	// 	Password:  "$2a$10$QYXnNZJH.hNuNOiT8Tq8nOJy02V0mcyT5h9ARQvA2bO35rdd72Zym",
-	// }
-	// users.CreateUserInDB(user)
-	// userDB, _ := users.GetUserDetails(body.Username)
-	// userResp := users.ConvertUserDBModelToUserResponse(userDB)
+	req, err := t_utils.MakeRequest(t_utils.POST, profiles.PROFILE_ROUTE_PREFIX, body)
+	if err != nil {
+		return
+	}
 
-	req, err := t_utils.MakeRequest(t_utils.POST, users.USER_ROUTE_PREFIX+"/loginuser", body)
+	// add the customed headers
+	for key, value := range customHeaders {
+		req.Header.Add(key, value)
+	}
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	err = json.Unmarshal(w.Body.Bytes(), &resp)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	log.Println("API Response", resp)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, expected.Status, resp.Status)
+	assert.Equal(t, expected.Message, resp.Message)
+	assert.Contains(t, resp.Data, "Profile")
+}
+
+func TestEditProfile(t *testing.T) {
+	var resp = common.APIResponse{}
+	var body = profiles.EditProfileRequest{
+		FirstName: "Albert",
+		LastName:  "Einstein",
+		Email:     "einsteinalbert@ufl.edu",
+		UserName:  "albert",
+	}
+	var expected = common.APIResponse{
+		Status:  http.StatusOK,
+		Message: common.API_SUCCESS,
+		Data:    map[string]interface{}{},
+	}
+
+	req, err := t_utils.MakeRequest(t_utils.PATCH, profiles.PROFILE_ROUTE_PREFIX, body)
 	if err != nil {
 		return
 	}
@@ -104,14 +129,14 @@ func TestLogin(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, expected.Status, resp.Status)
 	assert.Equal(t, expected.Message, resp.Message)
-	assert.Equal(t, expected.Data["user"], userResp)
+	assert.Contains(t, resp.Data, "updated")
 }
 
-func TestDeleteProfileForUser(t *testing.T) {
+func TestDeleteProfile(t *testing.T) {
 	var resp = common.APIResponse{}
 	var body = profiles.DeleteProfileRequest{
-		Username: "mary_jane",
-		Password: "password123",
+		Username: "albert",
+		Password: "special-relativity",
 	}
 	var expected = common.APIResponse{
 		Status:  http.StatusOK,
