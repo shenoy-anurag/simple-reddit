2b963298b618e3dc4e829d1f827dd8a819f97932
Manoj_Jampana <50446748+manojjampana@users.noreply.github.com>
2022-04-20 08:49:24 -0400

    refactor: Contact Info updated in Mod Policy

--- frontend/forum-app/src/app/modpolicy/modpolicy.component.ts
+++ frontend/forum-app/src/app/modpolicy/modpolicy.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Storage } from '../storage';
+import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-modpolicy',
@@ -8,6 +12,46 @@ import { Component, OnInit } from '@angular/core';
 export class ModpolicyComponent implements OnInit {
 
   constructor() { }
+
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+  
+  public showMyMessage = false
+  
+  showMessageSoon1() {
+    setTimeout(() => {
+      this.showMyMessage = true
+    }, 1000)
+  }
+  
+  showMessageSoon2() {
+    setTimeout(() => {
+      this.showMyMessage = true
+    }, 1500)
+  }
+  
+  showMessageSoon3() {
+    setTimeout(() => {
+      this.showMyMessage = true
+    }, 2000)
+  }
+  
+  showMessageSoon4() {
+    setTimeout(() => {
+      this.showMyMessage = true
+    }, 1500)
+  }
 
   ngOnInit(): void {
   }
